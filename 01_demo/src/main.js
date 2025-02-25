53248684760a7796431f6f6d114c46dbe132663f
liyang <995720636@qq.com>
2025-02-25 10:30:00 +0800

    复习

--- 01_demo/src/main.js
+++ 01_demo/src/main.js
@@ -2,6 +2,15 @@ import { createApp } from 'vue'
 // import App from './01_组件/App.vue'
 // import App from './02_组件通信_父传子/App.vue'
 // import App from './03_组件通信_子传父/App.vue'
-import App from './04_组件通信_案例练习/App.vue'
+// import App from './04_组件通信_案例练习/App.vue'
+// import App from './05_vue组件综合练习/App.vue'
+// import App from './06_组件插槽-基本使用/App.vue'
+// import App from './07_组件插槽-具名插槽/App.vue'
+// import App from './08_组件插槽-作用域插槽/App.vue'
+// import App from './09_Provide和Inject/App.vue'
+// import App from './10_事件总线的使用/App.vue'
+// import App from './11_动态组件/App.vue'
+import App from './12_动态组件-keeplive/App.vue'
+
 
 createApp(App).mount('#app')

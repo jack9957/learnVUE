68a3c1ee5c025472c7fa510f19710bdc6f91e001
Jack <995720636@qq.com>
2025-03-10 19:11:09 +0800

    学习

--- 07_learn_vuex/src/App.vue
+++ 07_learn_vuex/src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    首页计数: {{ $store.state.counter }}
+    <hr>
+    <Home></Home>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import Home from './views/Home.vue';
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
 </style>
---
+++ 07_learn_vuex/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app">
+    Home计数: {{ $store.state.counter }}
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+</style>
